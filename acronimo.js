ce14e8d639f3a0d1c557beda1deb3b2312a08f34
denisse <dlgon695@gmail.com>
2025-09-01 19:54:33 -0600

    feat: funcionalidad a los botones

---
+++ acronimo.js
@@ -0,0 +1,16 @@
+
+const buttonD = document.getElementById("Button-letter-D");
+const buttonE = document.getElementById("Button-letter-E");
+const buttonN = document.getElementById("Button-letter-N");
+
+buttonD.addEventListener("click", function(){
+    alert("\"Los datos son el nuevo petróleo\", pero más limpios y renovables. Cada click que haces en internet genera un dato, ¡y se producen más de 2.5 quintillones de bytes al día!");
+});
+
+buttonE.addEventListener("click", function(){
+    alert("Ese momento incómodo cuando buscas algo y la web te responde: \"Error 404-Página no encontrada\". Básicamente, internet diciendóte: \"Ups, no tengo idea de lo que buscas, suerte con eso.\"");
+});
+
+buttonN.addEventListener("click", function(){
+    alert("La nube no es un lugar mágico en el cielo, son servidores gigantes que guardan tus fotos, archivos y memes. Sin ella, no tendrías Netflix ni Spotify en cualquier dispositivo.");
+});
